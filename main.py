406b0a6c1b74a2176179e3a1e7491c9257879a2f
Jaturaphat Janseeda <77083612+Jjpj42@users.noreply.github.com>
2022-11-06 20:52:20 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask , render_template , request , Markup
+import flask
+from flask import *
 
 app= Flask(__name__)
 
@@ -10,15 +11,14 @@ def index():
 @app.route("/upload_Text", methods=["GET", "POST"])
 def uploadText():
     arr=[]
-    if request.method == 'POST':
-        plain_text = request.form['cipher-text']
-        number = len(plain_text)
-        for i in range(0,number):
+    plain_text = request.form['cipher-text']
+    number = len(plain_text)
+    for i in range(0,number):
+        arr.append(plain_text[i].lower())
+    print(arr)  
 
-            arr.append(plain_text[i].lower())
-        print(arr)
-    return render_template("index.html", arr=arr , Num=number)
+    return render_template("index.html", Arr=arr , len=len(plain_text))
 
 
 if __name__ == '__main__':
-    app.run(port=5500, debug = True)
+    app.run(port=5500 , debug = True)
